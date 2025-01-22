c21eacf262f5f1257f3aea466164df22dbfa4138
Johnson <oonnoonni@vectornav.com>
2025-01-22 16:50:01 -0600

    Starting Reversing Function

---
+++ reverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+	int reverser();
+	int status;
+	status = reverser("word");
+}
+
+int reverser(self)
+    char self[];
+{
+	int stringSize();
+    int size;
+    
+    size = stringSize(self);
+    printf(self);
+    printf("\nSize: %d\n",size);
+
+	return 0;
+}
+
+int stringSize(self)
+    char self[];
+{
+    int i = 0;
+    while (self[i] != '\0'){
+        i++;
+    }
+    return i;
+}
