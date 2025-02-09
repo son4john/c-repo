9d428f1f231aad433c1122e1e3693341926ceb85
johnson <johnson.oonnoonni@gmail.com>
2025-02-08 21:28:28 -0600

    calculator program

---
+++ calculator.c
@@ -0,0 +1,28 @@
+// Not Tested
+int main{
+    int a = 0;
+    int b = 5;
+    int c = 3;
+    int d = 2;    
+    int anwser;
+
+    anwser = addition(b,c);
+    printf("Add %d\n", awnser);
+}
+
+int addition(int num1, int num2){
+    return num1 + num2;
+}
+
+int subtraction(int num1, int num2){
+    return num1 - num2;
+}
+
+// Divison expection zero
+
+// Matrix additon
+// Matrix subtraction
+// Matrix multiplication
+
+// binary conversion
+// Hex COnversion
